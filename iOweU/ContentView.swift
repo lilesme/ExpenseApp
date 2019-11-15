0bc187727dd90710f3283d547eedc1da1c0dd96d
Megan <lilesme@oregonstate.edu>
2019-11-15 13:42:29 -0800

    Initial Commit

---
+++ iOweU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOweU
+//
+//  Created by Megan Liles on 11/15/19.
+//  Copyright © 2019 Megan Liles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
